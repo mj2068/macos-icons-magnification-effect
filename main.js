5a4bf9cbe0bf3c7778cda56511afa8e4c872b334
mj2068 <yuelianghushenfu@sina.com>
2022-11-17 18:45:35 +0800

    implemented, but not satisfied with the math

--- main.js
+++ main.js
@@ -2,12 +2,39 @@ console.log("main.js");
 
 let container = document.getElementById("container");
 
-for (let index = 0; index < 6; index++) {
+for (let index = 0; index < 8; index++) {
   let icon = document.createElement("div");
   icon.classList.add("icon");
   container.appendChild(icon);
 }
 
-let bigIcon = document.createElement("div");
-bigIcon.classList.add("big-icon");
-container.appendChild(bigIcon);
+// let bigIcon = document.createElement("div");
+// bigIcon.classList.add("big-icon");
+// container.appendChild(bigIcon);
+
+for (const icon of document.getElementsByClassName("icon")) {
+  icon.style.setProperty("--width", `${100}px`);
+  icon.style.setProperty("--height", `${100}px`);
+}
+container.addEventListener("mousemove", function (event) {
+  // console.log(event);
+  for (const icon of document.getElementsByClassName("icon")) {
+    // console.log(event.x - icon.offsetLeft);
+
+    let diff = event.x - (icon.offsetLeft + icon.clientWidth / 2);
+    let absDiff = Math.abs(diff);
+    if (absDiff <= 200) {
+      icon.style.setProperty(
+        "--width",
+        `${((200 - absDiff) / 200 + 1) * 100}px`
+      );
+      icon.style.setProperty(
+        "--height",
+        `${((200 - absDiff) / 200 + 1) * 100}px`
+      );
+    } else {
+      icon.style.setProperty("--width", `${100}px`);
+      icon.style.setProperty("--height", `${100}px`);
+    }
+  }
+});
