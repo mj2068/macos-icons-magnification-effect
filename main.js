b91dab850de86b60e6e8ce461c37767507599354
mj2068 <yuelianghushenfu@sina.com>
2022-11-18 12:43:05 +0800

    make range a variable

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ for (const icon of document.getElementsByClassName("icon")) {
   icon.style.setProperty("--width", `${100}px`);
   icon.style.setProperty("--height", `${100}px`);
 }
+
 container.addEventListener("mousemove", function (event) {
   // console.log(event);
   for (const icon of document.getElementsByClassName("icon")) {
@@ -23,14 +24,15 @@ container.addEventListener("mousemove", function (event) {
 
     let diff = event.x - (icon.offsetLeft + icon.clientWidth / 2);
     let absDiff = Math.abs(diff);
-    if (absDiff <= 200) {
+    let range = 500;
+    if (absDiff <= range) {
       icon.style.setProperty(
         "--width",
-        `${((200 - absDiff) / 200 + 1) * 100}px`
+        `${((range - absDiff) / range + 1) * 100}px`
       );
       icon.style.setProperty(
         "--height",
-        `${((200 - absDiff) / 200 + 1) * 100}px`
+        `${((range - absDiff) / range + 1) * 100}px`
       );
     } else {
       icon.style.setProperty("--width", `${100}px`);
